a73f83fb3df98a3520ddcf593deb04c3c54a155c
lchristine <xristinamarialagou@gmail.com>
2022-05-23 15:48:11 +0300

    1/3 of project

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.ImageKey = "(none)";
+            this.button1.Location = new System.Drawing.Point(843, 115);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(145, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "computer";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(844, 67);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 24);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(357, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TIC TAC TOE";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(729, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Give the N :";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gray;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(683, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "player";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(906, 546);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(885, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Number of X\'s";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(885, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Number of O\'s";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(888, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(888, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 18);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.images__1_2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 646);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{   
+    public partial class Form1 : Form
+    {
+        Count count = new Count();
+
+         public static int cntGl = 0;
+        public class kkk
+        {
+            public static int q;
+        }
+       bool turn = true;// true=x and false=o .
+                         //bool against_computer = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Button[,] buttons;// = new Button[q, q];                
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int top = 80;
+            int left = 180;
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show(" give an odd number!");
+          
+
+            }
+            kkk.q = int.Parse(textBox1.Text);
+            buttons = new Button[kkk.q, kkk.q];
+
+            //Δημιουργία κουμπιών
+            if (kkk.q % 2 == 1 && kkk.q != 1 && kkk.q != 3)
+            {
+                for (int j = 0; j < kkk.q; j++)
+                {
+                    left = left + 52;
+                    top = 80;
+                    for (int i = 0; i < kkk.q; i++)
+                    {
+                        Button button = new Button();
+                        button.Size = new Size(50, 50);
+                        button.Left = left;
+                        button.Top = top;
+                        button.BackColor = System.Drawing.Color.Gray;
+                        button.Font = new Font("Arial", 16);
+                        Controls.Add(button);
+                        top += button.Height + 2;
+                        button.Click += new EventHandler(this.button_Click);
+                        buttons[i, j] = button;                      
+
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show(" give an odd number!");
+            }        
+
+        }
+        //Χ ή Ο και να παίζει ο υπολογιστής
+        void button_Click(object sender, EventArgs e)
+        {
+            int r1=0, r2=0;
+            Random rand = new Random();
+            Button button = sender as Button;  //Then you can access the Button properties. 
+
+            button.Text = "X";
+            button.Enabled = false;
+            do
+            {
+                if (cntGl >= (kkk.q * kkk.q) / 2) 
+                {
+                    break; //gia na mhn ginei atermon
+                }
+                r1 = rand.Next(0, kkk.q);
+                r2 = rand.Next(0, kkk.q);
+            } while (buttons[r1, r2].Text == "X" || buttons[r1, r2].Text == "O"); //oso einai true 
+
+
+            if (cntGl < (kkk.q * kkk.q) / 2)
+            {
+                buttons[r1, r2].Text = "O";
+                buttons[r1, r2].Enabled = false;
+            }
+            
+            cntGl++;
+            checkForWinner();
+
+        }
+       
+        
+         private void checkForWinner()
+         {
+            int cnt = 0;
+            int cntDx = 0;
+            int cntDo = 0;
+            int cntVdx = 0;
+            int cntVdo = 0;
+            bool iso = false;
+            
+            for (int i = 0; i < kkk.q; i++) {
+                int cnt1 = 0;
+                int cnt2 = 0;
+                for (int j = 0; j < kkk.q -1; j++)
+                {
+                    if ((buttons[i, j].Text == buttons[i, j+1].Text) && (buttons[i, j].Text == "X")) //gia tis grammmes me x
+                    {
+                        cnt1++;
+                    }
+                    if ((buttons[i, j].Text == buttons[i, j + 1].Text) && (buttons[i, j].Text == "O")) //gia tis grammmes me o
+                    {
+                        cnt2++;
+                    }
+                    if (i == j)//gia thn kuria diagwnio
+                    {
+                        if (buttons[i, j].Text == "X")
+                        {
+                            cntDx++;
+                        }
+                        if (buttons[i, j].Text == "O")
+                        {
+                            cntDo++;
+                        }
+                    }
+                    if (i == (kkk.q) - j - 1)// gia thn deutereboysa diagwnio
+                    {
+                        if (buttons[i, j].Text == "X")
+                            cntVdx++;
+                        if (buttons[i, j].Text == "O")
+                            cntVdo++;
+                    }
+                    
+                }
+          
+                if (cnt1 == kkk.q - 1)
+                {
+                    MessageBox.Show(" X wins , tic-tac-toe!");
+                    iso = true;
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                    this.Close();
+                }
+                if (cnt2 == kkk.q - 1)
+                {
+                    MessageBox.Show(" O  wins , tic-tac-toe!");
+                    iso = true;
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                    this.Close();
+                }
+                if ( (cntDx == kkk.q - 1 && buttons[kkk.q - 1, kkk.q - 1].Text == "X")|| (cntVdx == kkk.q - 1 && buttons[kkk.q - 1, 0].Text == "X"))
+                {
+                    MessageBox.Show(" x  wins , tic-tac-toe!");
+                    iso = true;
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                    this.Close();
+                }
+                if ((cntDo == kkk.q - 1 && buttons[kkk.q - 1, kkk.q - 1].Text == "O") || (cntVdo == kkk.q - 1 && buttons[kkk.q - 1, 0].Text == "O"))
+                {
+                    MessageBox.Show(" O  wins , tic-tac-toe!");
+                    iso = true;
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                    this.Close();
+                }
+                
+            }
+            //gia tis sthles
+           for (int j = 0; j < kkk.q; j++)
+            {
+               int cnt1 = 0;
+                int cnt2 = 0;
+                for (int i = 0; i < kkk.q - 1; i++)
+                    {
+                    if ((buttons[i, j].Text == buttons[i+1, j].Text) && ( buttons[i, j].Text=="X"))
+                    {
+                        cnt1++;
+                    }
+                    if ((buttons[i, j].Text == buttons[i + 1, j].Text) && (buttons[i, j].Text == "O"))
+                    {
+                        cnt2++;
+                    }
+                    }
+                if (cnt1 == kkk.q - 1)
+                {
+                    MessageBox.Show(" X  wins , tic-tac-toe!");
+                    iso = true;
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                    this.Close();
+                }
+                if (cnt2 == kkk.q - 1)
+                {
+                    MessageBox.Show(" O  wins , tic-tac-toe!");
+                    iso = true;
+                   
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                    this.Close();
+                }
+               
+            }
+            for (int i = 0; i < kkk.q; i++)
+            {
+                for (int j = 0; j < kkk.q; j++)
+                {
+                    if ((buttons[i, j].Text == "O") || (buttons[i, j].Text == "X"))
+                    {
+                        cnt++; //counter για να δουμε αν ολα τα κουμπια ειναι γεματα,βοηθαει για την ισοπαλια μετα 
+
+                    }
+                }
+            }
+            if ((cnt == kkk.q * kkk.q) && (iso == false))
+                {
+                MessageBox.Show("no one wins!!!");
+                
+                Form2 f2 = new Form2();
+                f2.Show();
+                this.Close();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int top = 80;
+            int left = 180;
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show(" give an odd number!");
+            }
+            kkk.q = int.Parse(textBox1.Text);
+            buttons = new Button[kkk.q, kkk.q];
+
+
+            if (kkk.q % 2 == 1 && kkk.q != 1 && kkk.q != 3)
+            {
+                for (int j = 0; j < kkk.q; j++)
+                {
+                    left = left + 52;
+                    top = 80;
+                    for (int i = 0; i < kkk.q; i++)
+                    {
+                        Button buttonn = new Button();
+                        buttonn.Size = new Size(50, 50);
+                        buttonn.Left = left;
+                        buttonn.Top = top;
+                        buttonn.BackColor = System.Drawing.Color.Gray;
+                        buttonn.Font = new Font("Arial", 16);
+                        Controls.Add(buttonn);
+                        top += buttonn.Height + 2;
+                        buttonn.Click += new EventHandler(this.buttonn_Click);
+                        buttons[i, j] = buttonn;                      
+
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show(" give an odd number!");
+            }        
+
+        }
+       
+            void buttonn_Click(object sender, EventArgs e)
+            {
+                Button button = sender as Button;
+
+
+            if (turn)           //Turn=true=X
+            {
+                button.Text = "X";
+                count.countuserx++;
+            }
+            else
+            { button.Text = "O";
+                count.countusero++;
+            }
+                
+                button.Enabled = false;
+                checkForWinner();
+
+
+                turn = !turn; // Turn =false=O (changes the true to false <=> kai to antistrofo)
+                button.Enabled = false; // not change value
+
+            label5.Text = count.countuserx.ToString();
+            label6.Text = count.countusero.ToString();
+        }
+           
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+ 
+    }
+
+
+
